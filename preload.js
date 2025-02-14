fb8326c93a1eafc6aed5bb0a20f84909ddaf4135
Deepak-Yadav-14 <deepakyadav1452004@gmail.com>
2025-02-14 15:32:09 +0530

    Initialised electron app with git and monaco editor

---
+++ preload.js
@@ -0,0 +1,18 @@
+const { contextBridge } = require('electron');
+
+// Expose any needed versions or APIs
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+});
+
+// Expose MonacoEnvironment for Monaco Web Worker loading
+contextBridge.exposeInMainWorld('MonacoEnvironment', {
+  getWorkerUrl: (moduleId, label) => {
+    return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+      self.MonacoEnvironment = { baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.44.0/min/' };
+      importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.44.0/min/vs/base/worker/workerMain.js');
+    `)}`;
+  }
+});
