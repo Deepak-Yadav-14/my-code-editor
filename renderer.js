6d7fbce30626f9495426f3343ba9344c19bd097a
chanchal2305 <sonichanchal872@gmail.com>
2025-03-10 12:54:18 +0530

    added title bar

--- renderer.js
+++ renderer.js
@@ -4,10 +4,29 @@ window.addEventListener('DOMContentLoaded', () => {
     require(['vs/editor/editor.main'], function() {
       monaco.editor.create(document.getElementById('editor'), {
         value: "// Welcome to Monaco Editor in Electron!",
-        language: "javascript",
+        language: "cpp",
         theme: "vs-dark",
         automaticLayout: true
       });
     });
   });
-  
+  //buttons
+  /*
+  const remote=require("electron").remote;
+var minimise=document.getElementById("minimise");
+var maximise=document.getElementById("maximise");
+var quit=document.getElementById("quit");
+
+minimise.addEventListener("click",minimiseApp);
+maximise.addEventListener("click",maximiseApp);
+quit.addEventListener("click",quitApp);
+function minimiseApp(){
+    remote.BrowserWindow.getFocusedWindow().minimize();
+}
+function maximiseApp(){
+    remote.BrowserWindow.getFocusedWindow().maximize();
+}
+function quitAppApp(){
+    remote.getCurrentWindow().close();
+}
+  */
