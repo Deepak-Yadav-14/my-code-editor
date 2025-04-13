19ee519e652c24c1fec4a17e09426567220ad0d4
Deepak-Yadav-14 <143217869+Deepak-Yadav-14@users.noreply.github.com>
2025-04-13 20:46:15 +0530

    css

--- Hello_World.js
+++
@@ -1,5 +0,0 @@
-// Welcome to Monaco Editor in Electron!
-console.log('Hello Wrld');
-for(let i=0; i<5; i++){
-    console.log(i);
-}
--- hello.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hello"<<endl;
-    cout<<"1,2,3";
-}
